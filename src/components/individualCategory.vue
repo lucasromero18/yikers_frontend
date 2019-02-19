d1091a90fb868ac36e2f72f7372395bbd808411f
unknown <lucasromero65@yahoo.com>
2019-02-18 23:52:18 -0600

    almost done with situation card structure

--- src/components/individualCategory.vue
+++ src/components/individualCategory.vue
@@ -25,8 +25,8 @@
           <v-card class="cardContainer">
             <v-card-title class="situationsContainer" primary-title>
             <div>
-              <div class="headline">Situation Title</div>
-              <h2 class="usernameSituation">username</h2>
+              <div class="usernameSituation">@username</div>
+              <h2 class="headline">Situation</h2>
               <span class="situationText">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
               quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
               aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem 
@@ -42,15 +42,25 @@
               </div>
               <div class="buttonContainer">
               <v-btn class="viewSolution" icon @click="show = !show">
-                <span>{{ show ? 'Close' : 'See *usernames* Solution' }}</span>
+                <span class="showNoShow">{{ show ? 'Close' : 'See *usernames* Solution' }}</span>
               </v-btn>
               </div>
             </v-card-actions>
             <v-slide-y-transition>
             <div>
               <v-card-text class="solutionText" v-show="show">
-              This is where the solution to the situation will populate.
-              <v-slider class="slider" v-model="value" step="1" thumb-label></v-slider>
+              <h1 id="solution">Solution</h1>
+              <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
+              quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
+              aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
+              quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
+              aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</span>
+              <h1 id="yikersScaleHeader">Rate this solution with the Yikers scale!</h1>
+              <v-slider class="slider" v-model="value" step="5" thumb-label></v-slider>
+              <v-flex class="commentInputContainer" xs6>
+              <v-textarea outline name="input-7-4" label="Leave a Comment" value=""></v-textarea>
+              <v-btn id="addComment">Add Comment</v-btn>
+              </v-flex>
             </v-card-text>
             </div>
             </v-slide-y-transition>
@@ -135,7 +145,7 @@ display: flex;
 
 .usernameSituation{
   font-family: 'Bangers', cursive !important;
-  letter-spacing: 2px;
+  letter-spacing: 1px;
 }
 
 .situationText{
@@ -143,8 +153,18 @@ display: flex;
   font-size: 20px;
 }
 
+.showNoShow{
+  font-family: 'Abel', sans-serif;
+}
+
 .buttonContainer{
   margin-left: 70%;
+}
+
+#solution{
+  font-family: 'Bangers', cursive !important;
+  color: #b71c1c;
+  font-size: 25px;
 }
 
 .solutionText{
@@ -152,8 +172,28 @@ display: flex;
   font-size: 20px;
 }
 
+#yikersScaleHeader{
+  font-size: 25px;
+   font-family: 'Bangers', cursive;
+   color: #b71c1c;
+   margin-left: 60%; 
+}
+
 .slider{
   width: 40%;
+  float: right;
+  margin-top: 5%;
+}
+
+.commentInputContainer{
+ width: 30%;
+ margin-top: 2%;
+}
+
+#addComment{
+  color: white;
+  background-color: #b71c1c;
+  font-family: 'Bangers', cursive;
 }
 
 
